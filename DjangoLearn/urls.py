5f851ac37a11d0777c5e7fda1236288383c2862c
xuxin <xuxin.lucky@gmail.com>
2014-11-10 21:05:51 +0800

    add cookie

--- DjangoLearn/urls.py
+++ DjangoLearn/urls.py
@@ -1,13 +1,27 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoLearn.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    
     url(r'^index/$', 'blog.views.index'),
     url(r'^index1/$', 'blog.views.index1'),
     url(r'^index2/$', 'blog.views.index2'),
+    
+    url(r'^blog/register/$', 'blog.views.register'),
+    
+    url(r'^online/register/$', 'online.views.register'),
+    url(r'^online/login/$', 'online.views.login'),
+    url(r'^online/index/$', 'online.views.index'),
+    url(r'^online/logout/$', 'online.views.logout'),
+    
+    
+    url(r'^online2/login/$', 'online2.views.login'),
+    url(r'^online2/index/$', 'online2.views.index'),
+    url(r'^online2/logout/$', 'online2.views.logout'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, render_to_response
 from django.template import loader, Context, Template
 from django.http import HttpResponse
+from django import forms
 
 # Create your views here.
 def index(req):
@@ -15,3 +18,24 @@ def index1(req):
     
 def index2(req):
     return render_to_response('index.html', {'uname': 'terry xu'})
+
+
+class UserForm(forms.Form):
+    userName = forms.CharField()
+    headImg = forms.FileField()
+
+def register(req):
+    if req.method == 'POST':
+        uf = UserForm(req.POST, req.FILES)
+        if uf.is_valid():
+            print uf.cleaned_data
+            print uf.cleaned_data['headImg'].name
+            print uf.cleaned_data['headImg'].size
+            fp = file('e:/' + uf.cleaned_data['headImg'].name, 'wb')
+            s = uf.cleaned_data['headImg'].read()
+            fp.write(s)
+            fp.close()
+            return HttpResponse('ok')
+    else:
+        uf = UserForm()
+    return render_to_response('register.html', {'uf': uf})
